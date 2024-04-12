7352366068b4fb2e3ae4fe3b9d72ee4969092ec3
Dominik Havlicek <dominik.havlicek@tset.com>
2024-04-12 14:28:38 +0200

    Added i18n and Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="bg-white dark:bg-gray-800 min-h-screen dark:text-white">
+    <AppHeader />
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+      <NuxtPage />
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en', 
+  }
 })
